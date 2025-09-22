1a84847fe6f932611168d0c4306fd7b899fe8a9d
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-09-22 23:21:34 +0530

    feat: enhance store API with GET method to check store registration status and improve error handling

--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -24,14 +24,14 @@ export async function POST(request) {
             return NextResponse.json({error: "missing store information"}, {status: 400})
         }
 
+//Check if store already exists
+
         const store = await prisma.store.findFirst({
             where: {
                 userId: userId
             }
         })
-
-//Check if store already exists
-
+        
         if(store) {
             return NextResponse.json({status: store.status})
         }
@@ -48,7 +48,7 @@ export async function POST(request) {
             return NextResponse.json({error: "username is taken"}, {status: 400})
         }
 
-        // Upload image to ImageKit
+// Upload image to ImageKit
 
         const buffer = Buffer.from(await image.arrayBuffer());
         const response = await imagekit.upload({
@@ -95,6 +95,29 @@ export async function POST(request) {
 
         return NextResponse.json({message: "applied, waiting for approval"})
         
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message }, {status: 400});
+    }
+}
+
+// CHECK IS USER HAVE ALREADY REGISTERD A STORE  IF YES THEN SEND STATUS TO STORE
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request)
+        const store = await prisma.store.findFirst({
+            where: {
+                userId: userId
+            }
+        })
+
+        if(store) {
+            return NextResponse.json({status: store.status})
+        }
+
+        return NextResponse.json({status: "not registered"})
+
     } catch (error) {
         console.error(error);
         return NextResponse.json({error: error.code || error.message }, {status: 400});
