d551e98b665246b91a27f9b19dbbc6275257fc53
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-09-24 20:24:08 +0530

    feat: add API route to check seller status and retrieve store information with error handling

---
+++ app/api/store/is-seller/route.js
@@ -0,0 +1,24 @@
+import authSeller from "@/middleware/authSeller";
+import { getAuth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request)
+        const isSeller = await authSeller(userId)
+
+        if (!isSeller) {
+            return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+        }
+
+        const storeInfo = await Prisma.store.findUnique({
+            where: { userID }
+        })
+
+        return NextResponse.json({ isSeller, storeInfo })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message }, { status: 400 })
+    }
+}
